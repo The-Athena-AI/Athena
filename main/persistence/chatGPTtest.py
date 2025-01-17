a44a68b4fe1a558fbe0ed56b648efa16ab0794a2
Jackson <jbw6165@rit.edu>
2025-01-17 13:42:55 -0500

    begin testing chatGPT

---
+++ main/persistence/chatGPTtest.py
@@ -0,0 +1,19 @@
+"""
+test for chatGPT before I begin working on Grading functionality
+"""
+
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "developer", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
