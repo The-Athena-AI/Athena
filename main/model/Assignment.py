fa3e17bb1b3f75eb9def94a273fc989cc95a16fb
Jackson <jbw6165@rit.edu>
2025-01-19 17:00:54 -0500

    start of methods

--- main/model/Assignment.py
+++ main/model/Assignment.py
@@ -6,25 +6,28 @@ id - used to get the actual file from the database
 rubricId - used to match up the assignment with its answer key
 name - name of the assignment. will likely only be used for front-end stuff
 teachers - a list of people who have permission to edit the file
+file - the actual file that contains the assignment
 
 Rubric:
 id - used to get the actual file from the database
 assigmentId - used to match up the answer key with its assignment
 name - name of the assignment. will likely only be used for front-end stuff
 teachers - a list of people who have permission to edit the file
+file - the actual file that contains the answer key
 
 may be added in the future:
 something will prob be added to store the questions and answers so we don't need to retrieve them from the database every time
 """
 
 class Assignment:
-    __slots__ = ["__id", "__rubricId", "__name", "__teachers"]
+    __slots__ = ["__id", "__rubricId", "__name", "__teachers", "__file"]
 
-    def __init__(self, id, rubricId, name, teacher):
+    def __init__(self, id, rubricId, name, teacher, file):
         self.__id = id
         self.__rubricId = rubricId
         self.__name = name
         self.__teachers = [teacher]
+        self.__file = file
 
     def get_id(self):
         return self.__id
@@ -34,20 +37,24 @@ class Assignment:
         return self.__name
     def get_teachers(self):
         return self.__teachers
-    
+    def get_file(self):
+        return self.__file
+
     def add_teacher(self, teacher_id):
         self.__teachers.append(teacher_id)
     def remove_teacher(self, teacher_id):
         self.__teachers.remove(teacher_id)
 
-class Rubric:
-    __slots__ = ["__id", "__assignmentId", "__name", "__teachers"]
 
-    def __init__(self, id, assignmentId, name, teacher):
+class Rubric:
+    __slots__ = ["__id", "__assignmentId", "__name", "__teachers", "__file"]
+
+    def __init__(self, id, assignmentId, name, teacher, file):
         self.__id = id
         self.__assignmentId = assignmentId
         self.__name = name
         self.__teachers = [teacher]
+        self.__file = file
 
     def get_id(self):
         return self.__id
@@ -57,7 +64,9 @@ class Rubric:
         return self.__name
     def get_teachers(self):
         return self.__teachers
-    
+    def get_file(self):
+        return self.__file
+
     def add_teacher(self, teacher_id):
         self.__teachers.append(teacher_id)
     def remove_teacher(self, teacher_id):
--- main/persistence/GradingFileDAO.py
+++ main/persistence/GradingFileDAO.py
@@ -9,7 +9,28 @@ from openai import OpenAI
 client = OpenAI()
 
 def get_assignment(assignment_id):
-    return Assignment.Assignment(assignment_id, "assignment_name", "teacher_id")
+    """
+    get assignment info from database
+    get assignment file from database and store as string
+
+    create assignment object
+
+    return assignment object
+    """
 
 def get_rubric(rubric_id):
-    return Assignment.Rubric(rubric_id, "rubric_name", "teacher_id")
+    """
+    get rubric info from database
+    get rubric file from database and store as string
+
+    create rubric object
+
+    return rubric object
+    """
+
+def get_assignment_file(assignment):
+    return assignment.get_file()
+
+def get_rubric_file(rubric):
+    return rubric.get_file()
+
