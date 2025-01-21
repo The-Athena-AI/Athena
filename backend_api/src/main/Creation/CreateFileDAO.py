22aa843698190024ed21ec97d73ea11e325d2869
Jackson <jbw6165@rit.edu>
2025-01-20 22:43:57 -0500

    progress

--- backend_api/src/main/Creation/CreateFileDAO.py
+++ backend_api/src/main/Creation/CreateFileDAO.py
@@ -1,31 +1,16 @@
-"""
-File to use for uploading files and info to the database
-"""
 import supabase
-import pymongo
 
 # list of keys
 supabase_url = "https://wtgulmijdydgulpqhrfb.supabase.co"
 supabase_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0Z3VsbWlqZHlkZ3VscHFocmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5NTUxNDMsImV4cCI6MjA1MTUzMTE0M30.DXzDEFfh6mg07vwhnjrBSPUOJVesLzqRxC6QIfk3mf8"
-mongoDBuri = "mongodb://localhost:27017/"
 
 # list of clients
 supabase_client = supabase.create_client(supabase_url, supabase_api_key)
-mongoDB_client = pymongo.MongoClient(mongoDBuri)
-
-# mongoDB collections
-db = mongoDB_client["AthenasLibrary"]
-collectionA = db["Assignments"]
-collectionR = db["Rubrics"]
 
 def upload_assignment(assignment):
     # upload assignment info to supabase
-    supabase_client.table("Assignments").insert({"id": assignment.get_id(), "rubric id": None, "name": assignment.get_name(), "teachers": None}).execute()
-    # upload assignment file to mongoDB
-    collectionA.insert_one({"id": assignment.get_id(), "file": assignment.get_file()})
+    supabase_client.table("Assignments").insert({"id": assignment.get_id(), "rubric id": None, "name": assignment.get_name(), "teachers": None, "file": assignment.get_file()}).execute()
 
 def upload_rubric(rubric):
     # upload rubric info to supabase
-    supabase_client.table("Rubrics").insert({"id": rubric.get_id(), "assignment id": None, "name": rubric.get_name(), "teachers": None}).execute()
-    # upload rubric file to mongoDB
-    collectionR.insert_one({"id": rubric.get_id(), "file": rubric.get_file()})
+    supabase_client.table("Rubrics").insert({"id": rubric.get_id(), "assignment id": None, "name": rubric.get_name(), "teachers": None, "file": rubric.get_file()}).execute()
---
+++ backend_api/src/main/Creation/docai_processing.py
@@ -0,0 +1,33 @@
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai
+
+
+PROJECT_ID = "athena-439002"
+LOCATION = "us"  # Format is 'us' or 'eu'
+PROCESSOR_ID = "aa9bc53cdb71f5b1"  # Create processor in Cloud Console
+
+# Instantiates a client
+docai_client = documentai.DocumentProcessorServiceClient(
+    client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
+)
+
+# The full resource name of the processor, e.g.:
+# projects/project-id/locations/location/processor/processor-id
+# You must create new processors in the Cloud Console first
+RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
+
+def process_document(file):    
+    # Read the file into memory
+    image_content = file.read()
+
+    # Load Binary Data into Document AI RawDocument Object
+    raw_document = documentai.RawDocument(content=image_content, mime_type="application/pdf")
+
+    # Configure the process request
+    request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
+
+    # Use the Document AI client to process the sample form
+    result = docai_client.process_document(request=request)
+
+    document_object = result.document
+    return document_object.text
