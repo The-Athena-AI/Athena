6c60fa6cd2f9194fd56cc17726161fe4aa7ad4c6
Jackson <jbw6165@rit.edu>
2025-02-01 03:14:02 -0500

    grade should be complete for sunday

--- backend_api/src/main/Grading/GradingController.py
+++ backend_api/src/main/Grading/GradingController.py
@@ -11,17 +11,20 @@ supabase_api_key = os.getenv("SUPABASE_API_KEY")
 supabase_client = supabase.create_client(supabase_url, supabase_api_key)
 
 def grade():
+    completed_assignment_path = request.json['completed_assignment_path']
     assignment_id = request.json['assignment_id']
-    completed_assignment_url = request.json['completed_assignment_url']
     student_id = request.json['student_id']
+    submission_id = request.json['submission_id']
 
-    completed_assignment = supabase_client.storage.from_("assignments").download("1737950739841_tobe175zf1k.pdf");
+    completed_assignment = supabase_client.storage.from_("assignments").download(completed_assignment_path);
     
-    with open("documents/asdf.pdf", "wb+") as file:
+    with open("documents/completed_assignment.pdf", "wb+") as file:
         file.write(completed_assignment)
     
     completed_file = docai.process_document(file)
     assignment = DAO.get_assignment(assignment_id)
 
-    grade = DAO.grade_assignment(completed_file, assignment.get_rubric(), student_id)
+    grade = DAO.grade_assignment(completed_file, assignment, student_id, submission_id)
     return jsonify(grade)
+
+# completed_assignment_path: folder/file_name
--- backend_api/src/main/Grading/GradingFileDAO.py
+++ backend_api/src/main/Grading/GradingFileDAO.py
@@ -9,19 +9,39 @@ import re
 from dotenv import load_dotenv
 load_dotenv()
 
+example_output = """
+{
+    "grade": 85,
+    "overview": "You did not follow the instructions correctly.",
+    "feedback_strengths": [
+        {"questions/lines": "1-5", "rubric lines": "1-5", "feedback": "your hook was good."},
+        {"questions/lines": "11-20", "rubric lines": "11-20", "feedback": "your thesis statement was good and set up the essay well."}
+    ],
+    "feedback_weaknesses": [
+        {"questions/lines": "6-10", "rubric lines": "6-10", "feedback": "your summary was incorrect."},
+        {"questions/lines": "21-30", "rubric lines": "21-30", "feedback": "your topic sentence for the seccond paragraph was not good."}
+    ],
+}
+"""
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model=genai.GenerativeModel(
   model_name="gemini-1.5-flash",
   system_instruction="""
   You are a helpful assistant grading an assignment based on a rubric or answer key.
-  You will respond with a JSON object with the following keys: 'grade' and 'feedback'.
-  The grade should be a number between 0 and 100. The feedback should be an array of tuples with the following keys: 'questions/lines', 'rubric lines', 'feedback'.
+  You will respond with a JSON object with the following keys: 'grade', 'overview', 'feedback_strengths', 'feedback_weaknesses'.
+  The grade should be a number between 0 and 100.
+  The overview should be a string that gives an overview of why you gave the grade.
+  The feedback_strengths should be an array of tuples with the following keys: 'questions/lines', 'rubric lines', 'feedback'.
   The questions/lines should be what the feedback is applies to.
   The rubric lines should be the area of the rubric that the questions/lines are getting wrong.
-  The feedback should be a string that is a summary of what the student did wrong and how they can improve.
-  Please return the grade and feedback in **pure JSON format**, without markdown formatting.
-  Example format:
-  '{"grade": 85, "feedback": [{"questions/lines": "Line 1-10", "rubric lines": "Line 1-10", "feedback": "Good work!"}]}'
+  The feedback in this array should be a string that is a summary of what the student did well.
+  The feedback_weaknesses should be an array of tuples with the following keys: 'questions/lines', 'rubric lines', 'feedback'.
+  The questions/lines should be what the feedback is applies to.
+  The rubric lines should be the area of the rubric that the questions/lines are getting wrong.
+  The feedback in this array should be a string that is a summary of what the student did wrong.
+  Please return the grade, overview, and feedback in **pure JSON format**, without markdown formatting.
+  Example format: {example_output}
   """
 )
 
@@ -41,8 +61,8 @@ def get_assignment(assignment_id):
     assignment = Files.Assignment(assignment_id, data[0]["class_id"], data[0]["rubric"], data[0]["file"])
     return assignment
 
-def grade_assignment(completed_assignment, rubric, student_id):
-    response = model.generate_content(f"Here is the assignment: {completed_assignment}\nHere is the rubric or answer key: {rubric}")
+def grade_assignment(completed_assignment, assignment, student_id, submission_id):
+    response = model.generate_content(f"Here is the assignment: {completed_assignment}\nHere is the rubric or answer key: {assignment.get_rubric()}")
 
     if not response.text.strip():  # Check if response is empty
         raise ValueError("Empty response from AI model. Check your model output.")
@@ -56,28 +76,35 @@ def grade_assignment(completed_assignment, rubric, student_id):
         raise ValueError(f"Invalid JSON response: {cleaned_text}") from e
 
     grade = data["grade"]
-    feedback = data["feedback"]
+    overview = data["overview"]
+    feedback_strengths = data["feedback_strengths"]
+    feedback_weaknesses = data["feedback_weaknesses"]
     
-    submission = {
-        "assignment_id": "1352a4b8-1323-4b63-ad29-656c5e367a28",
-        "student_id": student_id,
-        "ai_grade": grade,
-        "ai_feedback": feedback,
-    }
-    submission_id = supabase_client.table("SubmittedAssignment").insert(submission).execute()
+    if submission_id:
+        submission = {
+            "id": submission_id,
+            "assignment_id": assignment.get_id(),
+            "student_id": student_id,
+            "ai_grade": grade,
+            "ai_overview": overview,
+            "ai_feedback_strengths": feedback_strengths,
+            "ai_feedback_weaknesses": feedback_weaknesses,
+        }
+    else:
+        submission = {
+            "assignment_id": assignment.get_id(),
+            "student_id": student_id,
+            "ai_grade": grade,
+            "ai_overview": overview,
+            "ai_feedback_strengths": feedback_strengths,
+            "ai_feedback_weaknesses": feedback_weaknesses,
+        }
+        
+    returned_id = supabase_client.table("SubmittedAssignment").upsert(submission, returning="id").execute()
 
     string = response.text
-    string += f"\nsubmission_id: {submission_id}"
+    string += f"\nsubmission_id: {returned_id}"
 
-    return response.text
+    print(string)
 
-"""
-{
-    "grade": 85,
-    "feedback": [
-        {"questions/lines": "1-5", "rubric lines": "1-5", "feedback": "You did not follow the instructions correctly."},
-        {"questions/lines": "6-10", "rubric lines": "1-5", "feedback": "You did not follow the instructions correctly."}
-    ]
-    "submission_id": "123"
-}
-"""
+    return string
