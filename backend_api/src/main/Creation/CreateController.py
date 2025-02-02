82a15b9af07984abef11111d4e8ef5320eb53540
Prajnyique Ghimire <prajnyiqueghimire@gmail.com>
2025-02-02 07:26:57 -0500

    Deleted random backend

--- backend_api/src/main/Creation/CreateController.py
+++
@@ -1,8 +0,0 @@
-from flask import request, jsonify
-from Athena.backend_api.src.main.Creation import CreateFileDAO as DAO
-
-def upload_assignment():
-    file = request.data
-    assignment = DAO.get_file_info(file)
-    DAO.upload_assignment(assignment)
-    return jsonify(assignment)
